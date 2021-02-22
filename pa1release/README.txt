39baf7a58de0e17ed5875b235adacb26dd309b7b
Catherine Horng <horngcatherine@gmail.com>
2021-02-22 14:23:20 -0800

    push release files

---
+++ pa1release/README.txt
@@ -0,0 +1,8 @@
+The project instructions are hosted on the web at:
+
+http://www.cs.cornell.edu/courses/cs4787/2019sp/projects/pa1/
+
+
+Setup instructions: to install the required python packages, run
+
+pip3 install -r requirements.txt
---
+++ pa1release/main.py
@@ -0,0 +1,156 @@
+from scipy.special import softmax
+from tqdm import tqdm
+from matplotlib import pyplot
+import os
+import numpy
+import scipy
+import matplotlib
+import mnist
+import pickle
+matplotlib.use('agg')
+
+mnist_data_directory = os.path.join(os.path.dirname(__file__), "data")
+
+# additional imports you may find useful for this assignment
+
+# TODO add any additional imports and global variables
+
+
+def load_MNIST_dataset():
+    PICKLE_FILE = os.path.join(mnist_data_directory, "MNIST.pickle")
+    try:
+        dataset = pickle.load(open(PICKLE_FILE, 'rb'))
+    except:
+        # load the MNIST dataset
+        mnist_data = mnist.MNIST(mnist_data_directory,
+                                 return_type="numpy", gz=True)
+        Xs_tr, Lbls_tr = mnist_data.load_training()
+        Xs_tr = Xs_tr.transpose() / 255.0
+        Ys_tr = numpy.zeros((10, 60000))
+        for i in range(60000):
+            Ys_tr[Lbls_tr[i], i] = 1.0  # one-hot encode each label
+        Xs_te, Lbls_te = mnist_data.load_testing()
+        Xs_te = Xs_te.transpose() / 255.0
+        Ys_te = numpy.zeros((10, 10000))
+        for i in range(10000):
+            Ys_te[Lbls_te[i], i] = 1.0  # one-hot encode each label
+
+        dataset = (Xs_tr, Ys_tr, Xs_te, Ys_te)
+        pickle.dump(dataset, open(PICKLE_FILE, 'wb'))
+    return dataset
+
+
+# compute the cross-entropy loss of the classifier
+#
+# x         examples          (d)
+# y         labels            (c)
+# gamma     L2 regularization constant
+# W         parameters        (c * d)
+#
+# returns   the model cross-entropy loss
+def multinomial_logreg_loss_i(x, y, gamma, W):
+    # TODO students should implement this in Part 1
+
+# compute the gradient of a single example of the multinomial logistic regression objective, with regularization
+#
+# x         training example   (d)
+# y         training label     (c)
+# gamma     L2 regularization constant
+# W         parameters        (c * d)
+#
+# returns   the gradient of the model parameters
+
+
+def multinomial_logreg_grad_i(x, y, gamma, W):
+    # TODO students should implement this in Part 1
+
+# test that the function multinomial_logreg_grad_i is indeed the gradient of multinomial_logreg_loss_i
+
+
+def test_gradient():
+    # TODO students should implement this in Part 1
+
+# compute the error of the classifier
+#
+# Xs        examples          (d * n)
+# Ys        labels            (c * n)
+# W         parameters        (c * d)
+#
+# returns   the model error as a percentage of incorrect labels
+
+
+def multinomial_logreg_error(Xs, Ys, W):
+    # TODO students should implement this
+
+# compute the gradient of the multinomial logistic regression objective, with regularization
+#
+# Xs        training examples (d * n)
+# Ys        training labels   (c * n)
+# gamma     L2 regularization constant
+# W         parameters        (c * d)
+#
+# returns   the gradient of the model parameters
+
+
+def multinomial_logreg_total_grad(Xs, Ys, gamma, W):
+    # TODO students should implement this
+    # a starter solution using an average of the example gradients
+    (d, n) = Xs.shape
+    acc = W * 0.0
+    for i in range(n):
+        acc += multinomial_logreg_grad_i(Xs[:, i], Ys[:, i], gamma, W)
+    return acc / n
+
+# compute the cross-entropy loss of the classifier
+#
+# Xs        examples          (d * n)
+# Ys        labels            (c * n)
+# gamma     L2 regularization constant
+# W         parameters        (c * d)
+#
+# returns   the model cross-entropy loss
+
+
+def multinomial_logreg_total_loss(Xs, Ys, gamma, W):
+    # TODO students should implement this
+    # a starter solution using an average of the example gradients
+    (d, n) = Xs.shape
+    acc = 0.0
+    for i in range(n):
+        acc += multinomial_logreg_loss_i(Xs[:, i], Ys[:, i], gamma, W)
+    return acc / n
+
+
+# run gradient descent on a multinomial logistic regression objective, with regularization
+#
+# Xs            training examples (d * n)
+# Ys            training labels   (d * c)
+# gamma         L2 regularization constant
+# W0            the initial value of the parameters (c * d)
+# alpha         step size/learning rate
+# num_iters     number of iterations to run
+# monitor_freq  how frequently to output the parameter vector
+#
+# returns       a list of models parameters, one every "monitor_freq" iterations
+#               should return model parameters before iteration 0, iteration monitor_freq, iteration 2*monitor_freq, and again at the end
+#               for a total of (num_iters/monitor_freq)+1 models, if num_iters is divisible by monitor_freq.
+def gradient_descent(Xs, Ys, gamma, W0, alpha, num_iters, monitor_freq):
+    # TODO students should implement this
+
+
+# estimate the error of the classifier
+#
+# Xs        examples          (d * n)
+# Ys        labels            (c * n)
+# gamma     L2 regularization constant
+# W         parameters        (c * d)
+# nsamples  number of samples to use for the estimation
+#
+# returns   the estimated model error when sampling with replacement
+def estimate_multinomial_logreg_error(Xs, Ys, W, nsamples):
+    # TODO students should implement this
+
+
+if __name__ == "__main__":
+    (Xs_tr, Ys_tr, Xs_te, Ys_te) = load_MNIST_dataset()
+    # TODO add code to produce figures
