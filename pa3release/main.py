fe3baf32bb49721aa29a133dc4412cf00987d7d5
Madscba <57229906+Madscba@users.noreply.github.com>
2021-04-02 17:07:01 -0400

    pa3 release

---
+++ pa3release/main.py
@@ -0,0 +1,169 @@
+#!/usr/bin/env python3
+import os
+import numpy
+from numpy import random
+import scipy
+import matplotlib
+import mnist
+import pickle
+matplotlib.use('agg')
+from matplotlib import pyplot
+
+from scipy.special import softmax
+
+mnist_data_directory = os.path.join(os.path.dirname(__file__), "data")
+
+# TODO add any additional imports and global variables
+
+
+def load_MNIST_dataset():
+    PICKLE_FILE = os.path.join(mnist_data_directory, "MNIST.pickle")
+    try:
+        dataset = pickle.load(open(PICKLE_FILE, 'rb'))
+    except:
+        # load the MNIST dataset
+        mnist_data = mnist.MNIST(mnist_data_directory, return_type="numpy", gz=True)
+        Xs_tr, Lbls_tr = mnist_data.load_training();
+        Xs_tr = Xs_tr.transpose() / 255.0
+        Ys_tr = numpy.zeros((10, 60000))
+        for i in range(60000):
+            Ys_tr[Lbls_tr[i], i] = 1.0  # one-hot encode each label
+        # shuffle the training data
+        numpy.random.seed(8675309)
+        perm = numpy.random.permutation(60000)
+        Xs_tr = numpy.ascontiguousarray(Xs_tr[:,perm])
+        Ys_tr = numpy.ascontiguousarray(Ys_tr[:,perm])
+        Xs_te, Lbls_te = mnist_data.load_testing();
+        Xs_te = Xs_te.transpose() / 255.0
+        Ys_te = numpy.zeros((10, 10000))
+        for i in range(10000):
+            Ys_te[Lbls_te[i], i] = 1.0  # one-hot encode each label
+        Xs_te = numpy.ascontiguousarray(Xs_te)
+        Ys_te = numpy.ascontiguousarray(Ys_te)
+        dataset = (Xs_tr, Ys_tr, Xs_te, Ys_te)
+        pickle.dump(dataset, open(PICKLE_FILE, 'wb'))
+    return dataset
+
+
+# compute the gradient of the multinomial logistic regression objective, with regularization (SAME AS PROGRAMMING ASSIGNMENT 2)
+#
+# Xs        training examples (d * n)
+# Ys        training labels   (c * n)
+# ii        the list/vector of indexes of the training example to compute the gradient with respect to
+# gamma     L2 regularization constant
+# W         parameters        (c * d)
+#
+# returns   the average gradient of the regularized loss of the examples in vector ii with respect to the model parameters
+def multinomial_logreg_grad_i(Xs, Ys, ii, gamma, W):
+    # TODO students should use their implementation from programming assignment 2
+
+
+# compute the error of the classifier (SAME AS PROGRAMMING ASSIGNMENT 1)
+#
+# Xs        examples          (d * n)
+# Ys        labels            (c * n)
+# W         parameters        (c * d)
+#
+# returns   the model error as a percentage of incorrect labels
+def multinomial_logreg_error(Xs, Ys, W):
+    # TODO students should use their implementation from programming assignment 1
+
+
+# compute the cross-entropy loss of the classifier
+#
+# Xs        examples          (d * n)
+# Ys        labels            (c * n)
+# gamma     L2 regularization constant
+# W         parameters        (c * d)
+#
+# returns   the model cross-entropy loss
+def multinomial_logreg_loss(Xs, Ys, gamma, W):
+    # TODO students should implement this
+
+
+# gradient descent (SAME AS PROGRAMMING ASSIGNMENT 1)
+#
+# Xs              training examples (d * n)
+# Ys              training labels   (c * n)
+# gamma           L2 regularization constant
+# W0              the initial value of the parameters (c * d)
+# alpha           step size/learning rate
+# num_epochs      number of epochs (passes through the training set, or equivalently iterations of gradient descent) to run
+# monitor_period  how frequently, in terms of epochs/iterations to output the parameter vector
+#
+# returns         a list of model parameters, one every "monitor_period" epochs
+def gradient_descent(Xs, Ys, gamma, W0, alpha, num_epochs, monitor_period):
+    # TODO students should use their implementation from programming assignment 1
+
+
+# gradient descent with nesterov momentum
+#
+# Xs              training examples (d * n)
+# Ys              training labels   (c * n)
+# gamma           L2 regularization constant
+# W0              the initial value of the parameters (c * d)
+# alpha           step size/learning rate
+# beta            momentum hyperparameter
+# num_epochs      number of epochs (passes through the training set, or equivalently iterations of gradient descent) to run
+# monitor_period  how frequently, in terms of epochs/iterations to output the parameter vector
+#
+# returns         a list of model parameters, one every "monitor_period" epochs
+def gd_nesterov(Xs, Ys, gamma, W0, alpha, beta, num_epochs, monitor_period):
+    # TODO students should implement this
+
+
+# SGD: run stochastic gradient descent with minibatching and sequential sampling order (SAME AS PROGRAMMING ASSIGNMENT 2)
+#
+# Xs              training examples (d * n)
+# Ys              training labels   (c * n)
+# gamma           L2 regularization constant
+# W0              the initial value of the parameters (c * d)
+# alpha           step size/learning rate
+# B               minibatch size
+# num_epochs      number of epochs (passes through the training set) to run
+# monitor_period  how frequently, in terms of batches (not epochs) to output the parameter vector
+#
+# returns         a list of model parameters, one every "monitor_period" batches
+def sgd_minibatch_sequential_scan(Xs, Ys, gamma, W0, alpha, B, num_epochs, monitor_period):
+    # TODO students should use their implementation from programming assignment 2
+
+
+# SGD + Momentum: add momentum to the previous algorithm
+#
+# Xs              training examples (d * n)
+# Ys              training labels   (c * n)
+# gamma           L2 regularization constant
+# W0              the initial value of the parameters (c * d)
+# alpha           step size/learning rate
+# beta            momentum hyperparameter
+# B               minibatch size
+# num_epochs      number of epochs (passes through the training set) to run
+# monitor_period  how frequently, in terms of batches (not epochs) to output the parameter vector
+#
+# returns         a list of model parameters, one every "monitor_period" batches
+def sgd_mss_with_momentum(Xs, Ys, gamma, W0, alpha, beta, B, num_epochs, monitor_period):
+    # TODO students should implement this
+
+
+# Adam Optimizer
+#
+# Xs              training examples (d * n)
+# Ys              training labels   (c * n)
+# gamma           L2 regularization constant
+# W0              the initial value of the parameters (c * d)
+# alpha           step size/learning rate
+# rho1            first moment decay rate ρ1
+# rho2            second moment decay rate ρ2
+# B               minibatch size
+# eps             small factor used to prevent division by zero in update step
+# num_epochs      number of epochs (passes through the training set) to run
+# monitor_period  how frequently, in terms of batches (not epochs) to output the parameter vector
+#
+# returns         a list of model parameters, one every "monitor_period" batches
+def adam(Xs, Ys, gamma, W0, alpha, rho1, rho2, B, eps, num_epochs, monitor_period):
+    # TODO students should implement this
+
+
+if __name__ == "__main__":
+    (Xs_tr, Ys_tr, Xs_te, Ys_te) = load_MNIST_dataset()
+    # TODO add code to produce figures
