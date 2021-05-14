9db518482975d2d38132bb9f3c00dbbedd8156cd
Jonathan Moon <hm447@cornell.edu>
2021-05-14 12:24:39 -0400

    Finished pt4

--- pa6release/release/main.py
+++ pa6release/release/main.py
@@ -4,7 +4,7 @@ import os
 # BEGIN THREAD SETTINGS this sets the number of threads used by numpy in the program (should be set to 1 for Parts 1 and 3)
 import numpy as np
 
-implicit_num_threads = 1
+implicit_num_threads = 4
 os.environ["OMP_NUM_THREADS"] = str(implicit_num_threads)
 os.environ["MKL_NUM_THREADS"] = str(implicit_num_threads)
 os.environ["OPENBLAS_NUM_THREADS"] = str(implicit_num_threads)
@@ -304,15 +304,15 @@ def sgd_mss_with_momentum_noalloc_float32(Xs, Ys, gamma, W0, alpha, beta, B, num
     (d, n) = Xs.shape
     (c, d) = W0.shape
     # TODO students should initialize the parameter vector W and pre-allocate any needed arrays here
-    W = numpy.zeros(W0.shape, dtype='numpy.float32')
-    V = numpy.zeros(W0.shape, dtype='numpy.float32')
+    W = numpy.zeros(W0.shape, dtype=numpy.float32)
+    V = numpy.zeros(W0.shape, dtype=numpy.float32)
 
     # cb1, cb2, b1, cd1,cd2
 
-    cb1 = numpy.zeros((c, B), dtype='numpy.float32')
-    b1 = numpy.zeros((B), dtype='numpy.float32')
-    cd1 = numpy.zeros((c, d), dtype='numpy.float32')
-    cd2 = numpy.zeros((c, d), dtype='numpy.float32')
+    cb1 = numpy.zeros((c, B), dtype=numpy.float32)
+    b1 = numpy.zeros((B), dtype=numpy.float32)
+    cd1 = numpy.zeros((c, d), dtype=numpy.float32)
+    cd2 = numpy.zeros((c, d), dtype=numpy.float32)
 
     # intermediate_matrix2 = numpy.zeros((c, d))
     # XS_slice = numpy.ascontiguousarray(Xs[:, ii])
@@ -323,12 +323,12 @@ def sgd_mss_with_momentum_noalloc_float32(Xs, Ys, gamma, W0, alpha, beta, B, num
         Xs_splits.append(numpy.ascontiguousarray(Xs[:, i * B:i * B + B]))
         Ys_splits.append(Ys[:, i * B:i * B + B])
 
-    print("Running minibatch sequential-scan SGD with momentum (no allocation)")
+    print("Running minibatch sequential-scan SGD with momentum (no allocation) and float32")
     for it in tqdm(range(num_epochs)):
         for ibatch in range(int(n / B)):
             # TODO this section of code should only use numpy operations with the "out=" argument specified (students should implement this)
-            XS_split = Xs_splits[ibatch]
-            Ys_split = Ys_splits[ibatch]
+            XS_split = Xs_splits[ibatch].astype(numpy.float32)
+            Ys_split = Ys_splits[ibatch].astype(numpy.float32)
 
             numpy.dot(W, XS_split, out=cb1)  # (c,d) x (d,b) = (c,b) cb1
             numpy.amax(cb1, axis=0, out=b1)  # (c,b) cb2
@@ -366,11 +366,11 @@ def sgd_mss_with_momentum_threaded_float32(Xs, Ys, gamma, W0, alpha, beta, B, nu
     (d, n) = Xs.shape
     (c, d) = W0.shape
     # TODO perform any global setup/initialization/allocation (students should implement this)
-    W = numpy.zeros(W0.shape, dtype='numpy.float32')
-    V = numpy.zeros(W0.shape, dtype='numpy.float32')
+    W = numpy.zeros(W0.shape, dtype=numpy.float32)
+    V = numpy.zeros(W0.shape, dtype=numpy.float32)
     B_prime = int(B / num_threads)
-    gradients = numpy.zeros((num_threads, W0.shape[0], W0.shape[1]), dtype='numpy.float32')
-    sum_gradients = numpy.zeros(W0.shape, dtype='numpy.float32')
+    gradients = numpy.zeros((num_threads, W0.shape[0], W0.shape[1]), dtype=numpy.float32)
+    sum_gradients = numpy.zeros(W0.shape, dtype=numpy.float32)
     # construct the barrier object
     iter_barrier = threading.Barrier(num_threads + 1)
 
@@ -388,15 +388,15 @@ def sgd_mss_with_momentum_threaded_float32(Xs, Ys, gamma, W0, alpha, beta, B, nu
     def thread_main(ithread):
         # TODO perform any per-thread allocations
 
-        cb1 = numpy.zeros((c, B_prime), dtype='numpy.float32')
-        b1 = numpy.zeros((B_prime), dtype='numpy.float32')
-        cd1 = numpy.zeros((c, d), dtype='numpy.float32')
-        cd2 = numpy.zeros((c, d), dtype='numpy.float32')
+        cb1 = numpy.zeros((c, B_prime), dtype=numpy.float32)
+        b1 = numpy.zeros((B_prime), dtype=numpy.float32)
+        cd1 = numpy.zeros((c, d), dtype=numpy.float32)
+        cd2 = numpy.zeros((c, d), dtype=numpy.float32)
         for it in range(num_epochs):
             for ibatch in range(int(n / B)):
                 # TODO work done by thread in each iteration; this section of code should primarily use numpy operations with the "out=" argument specified (students should implement this)
-                Xs_split = Xs_splits[ibatch * num_threads + ithread]
-                Ys_split = Ys_splits[ibatch * num_threads + ithread]
+                Xs_split = Xs_splits[ibatch * num_threads + ithread].astype(numpy.float32)
+                Ys_split = Ys_splits[ibatch * num_threads + ithread].astype(numpy.float32)
 
                 numpy.dot(W, Xs_split, out=cb1)  # (c,d) x (d,b) = (c,b) cb1
                 numpy.amax(cb1, axis=0, out=b1)  # (c,b) cb2
@@ -420,7 +420,7 @@ def sgd_mss_with_momentum_threaded_float32(Xs, Ys, gamma, W0, alpha, beta, B, nu
         # print("running thread ", t)
         t.start()
 
-    print("Running threaded minibatch sequential-scan SGD with momentum (%d threads)" % num_threads)
+    print("Running threaded minibatch sequential-scan SGD with momentum (%d threads) and float32" % num_threads)
     for it in tqdm(range(num_epochs)):
         for ibatch in range(int(n / B)):
             iter_barrier.wait()
@@ -479,23 +479,32 @@ def plot_time_pt2(t1s_single_core, t2s_single_core, t1s_multi_core, t2s_multi_co
     # pyplot.show()
     # pyplot.clf()
 
-def plot_time_pt3(t1s, t2s, t3s, B_values, cores=1):
+def plot_time_pt3(ts_manual_threading, B_values, cores=1):
     pyplot.figure(figsize=(12, 8))
-    # pyplot.scatter(np.linspace(np.min(B_values), np.max(B_values), len(B_values)), t1s, label="t1", marker ='o')
-    # pyplot.scatter(np.linspace(np.min(B_values), np.max(B_values), len(B_values)), t2s, label="t2", marker='x')
-    # pyplot.scatter(np.linspace(np.min(B_values), np.max(B_values), len(B_values)), t3s, label="t3", marker='*')
-    t1_plot, = pyplot.plot(np.linspace(np.min(B_values), np.max(B_values), len(B_values)), t1s)
-    t1_plot.set_label('w/o preallocation & w/o manual threading')
-    t2_plot, = pyplot.plot(np.linspace(np.min(B_values), np.max(B_values), len(B_values)), t2s)
-    t2_plot.set_label('w/ preallocation & w/o manual threading')
-    t3_plot, = pyplot.plot(np.linspace(np.min(B_values), np.max(B_values), len(B_values)), t3s)
-    t3_plot.set_label('w/ preallocation & w/ manual threading')
+    t1s_single_core_filepath = 't1s_1_core.npy'
+    t2s_single_core_filepath = 't2s_1_core.npy'
+    t1s_multi_core_filepath = f't1s_4_core.npy'
+    t2s_multi_core_filepath = f't2s_4_core.npy'
+    t1s_single_core = numpy.load(t1s_single_core_filepath)
+    t2s_single_core = numpy.load(t2s_single_core_filepath)
+    t1s_multi_core = numpy.load(t1s_multi_core_filepath)
+    t2s_multi_core = numpy.load(t2s_multi_core_filepath)
+    t1_plot, = pyplot.plot(np.linspace(np.min(B_values), np.max(B_values), len(B_values)), t1s_single_core)
+    t1_plot.set_label(f"w/o preallocation w/ 1 core w/o manual threading")
+    t2_plot, = pyplot.plot(np.linspace(np.min(B_values), np.max(B_values), len(B_values)), t2s_single_core)
+    t2_plot.set_label(f"w/ preallocation w/ 1 core w/o manual threading")
+    t3_plot, = pyplot.plot(np.linspace(np.min(B_values), np.max(B_values), len(B_values)), t1s_multi_core)
+    t3_plot.set_label(f"w/o preallocation w/ 4 cores w/o manual threading")
+    t4_plot, = pyplot.plot(np.linspace(np.min(B_values), np.max(B_values), len(B_values)), t2s_multi_core)
+    t4_plot.set_label(f"w preallocation w/ 4 cores w/o manual threading")
+    t5_plot, = pyplot.plot(np.linspace(np.min(B_values), np.max(B_values), len(B_values)), ts_manual_threading)
+    t5_plot.set_label('w/ preallocation & w/ 1 core w/ manual threading')
     pyplot.title("Time comparisons of preallocation and threading")
     pyplot.xticks(np.linspace(np.min(B_values),np.max(B_values),len(B_values)),B_values)
     pyplot.xlabel("B_values")
     pyplot.ylabel("Time used (s)")
     pyplot.legend()
-    pyplot.savefig(f"pt2runtime_part_cores{cores}")
+    pyplot.savefig("Impact of Preallocation and Threading")
     pyplot.plot()
     # pyplot.show()
     # pyplot.clf()
@@ -534,14 +543,20 @@ def part12(Xs_tr, Ys_tr, Xs_te, Ys_te):
         t2s.append(t2)
     t1s = numpy.asarray(t1s)
     t2s = numpy.asarray(t2s)
-    t1s_single_core_filepath = 't1s_single_core.npy'
-    t2s_single_core_filepath = 't2s_single_core.npy'
-    numpy.save(t1s_single_core_filepath, t1s)
-    numpy.save(t2s_single_core_filepath, t2s)
-    # t1s_single_core = numpy.load(t1s_single_core_filepath)
-    # t2s_single_core = numpy.load(t2s_single_core_filepath)
+    t1s_filepath = f't1s_{implicit_num_threads}_core.npy'
+    t2s_filepath = f't2s_{implicit_num_threads}_core.npy'
+    numpy.save(t1s_filepath, t1s)
+    numpy.save(t2s_filepath, t2s)
+    t1s_single_core_filepath = 't1s_1_core.npy'
+    t2s_single_core_filepath = 't2s_1_core.npy'
+    t1s_multi_core_filepath = f't1s_4_core.npy'
+    t2s_multi_core_filepath = f't2s_4_core.npy'
+    t1s_single_core = numpy.load(t1s_single_core_filepath)
+    t2s_single_core = numpy.load(t2s_single_core_filepath)
+    t1s_multi_core = numpy.load(t1s_multi_core_filepath)
+    t2s_multi_core = numpy.load(t2s_multi_core_filepath)
 
-    # plot_time_pt2(t1s_single_core, t2s_single_core, t1s, t2s, B_values, 4)
+    plot_time_pt2(t1s_single_core, t2s_single_core, t1s_multi_core, t2s_multi_core, B_values, 4)
 
 def part3(Xs_tr, Ys_tr, Xs_te, Ys_te):
     (d, n) = Xs_tr.shape
@@ -553,38 +568,23 @@ def part3(Xs_tr, Ys_tr, Xs_te, Ys_te):
     n_threads = 4
     W0 = numpy.random.rand(c,d)
     B_values = [8,16,30,60,200,600,3000]
-    # B_values = [8,16]
-    t1s = []
-    t2s = []
     t3s = []
     for B_size in B_values:
         B = B_size
         print("Batch size: ", B_size)
-        t1 = time.time()
-        model1_w = sgd_mss_with_momentum(Xs_tr, Ys_tr, gamma, W0, alpha, beta, B, num_epochs)
-        t1 = time.time() - t1
-        print("\ttime for sgd_mss_with_momentum:", t1)
-
-        t2 = time.time()
-        model2_w = sgd_mss_with_momentum_noalloc(Xs_tr, Ys_tr, gamma, W0, alpha, beta, B, num_epochs)
-        t2 = time.time() - t2
-        print("\ttime for sgd_mss_with_momentum_noalloc:", t2)
 
         t3 = time.time()
         model3_w = sgd_mss_with_momentum_threaded(Xs_tr, Ys_tr, gamma, W0, alpha, beta, B, num_epochs, n_threads)
         t3 = time.time() - t3
         print("\ttime for sgd_mss_with_momentum_threaded:", t3)
 
-        err1 = multinomial_logreg_error(Xs_te, Ys_te, model1_w)
-        err2 = multinomial_logreg_error(Xs_te, Ys_te, model2_w)
         err3 = multinomial_logreg_error(Xs_te, Ys_te, model3_w)
-        print("\terror1: ", err1, ".\n\terror2 ", err2, "\n\terror3 ", err3)
-
-        t1s.append(t1)
-        t2s.append(t2)
+        print("\terror1: ", err3)
         t3s.append(t3)
-
-    plot_time_pt3(t1s, t2s, t3s, B_values)
+    t3s = numpy.asarray(t3s)
+    t3s_filepath = f'manual_threaded.npy'
+    numpy.save(t3s_filepath, t3s)
+    plot_time_pt3(t3s, B_values)
 
 def part4(Xs_tr, Ys_tr, Xs_te, Ys_te):
     (d, n) = Xs_tr.shape
@@ -593,8 +593,8 @@ def part4(Xs_tr, Ys_tr, Xs_te, Ys_te):
     beta = 0.9
     gamma = 0.0001
     num_epochs = 20
-    n_threads = 4
     W0 = numpy.random.rand(c,d)
+    n_threads = 4
     B_values = [8,16,30,60,200,600,3000]
     # B_values = [8,16]
     t1s = []
@@ -604,35 +604,80 @@ def part4(Xs_tr, Ys_tr, Xs_te, Ys_te):
         B = B_size
         print("Batch size: ", B_size)
         t1 = time.time()
-        model1_w = sgd_mss_with_momentum_noalloc_float32(Xs_tr, Ys_tr, gamma, W0, alpha, beta, B, num_epochs)
+        model1_w = sgd_mss_with_momentum_threaded_float32(Xs_tr, Ys_tr, gamma, W0, alpha, beta, B, num_epochs,n_threads)
         t1 = time.time() - t1
-        print("\ttime for sgd_mss_with_momentum:", t1)
+        print("\ttime for sgd_mss_with_momentum_noalloc_float32:", t1)
 
         t2 = time.time()
-        model2_w = sgd_mss_with_momentum_noalloc(Xs_tr, Ys_tr, gamma, W0, alpha, beta, B, num_epochs)
+        # model2_w = sgd_mss_with_momentum_noalloc_float32(Xs_tr, Ys_tr, gamma, W0, alpha, beta, B, num_epochs)
         t2 = time.time() - t2
-        print("\ttime for sgd_mss_with_momentum_noalloc:", t2)
-
-        t3 = time.time()
-        model3_w = sgd_mss_with_momentum_threaded(Xs_tr, Ys_tr, gamma, W0, alpha, beta, B, num_epochs, n_threads)
-        t3 = time.time() - t3
-        print("\ttime for sgd_mss_with_momentum_threaded:", t3)
+        print("\ttime for sgd_mss_with_momentum_threaded_float32:", t2)
 
         err1 = multinomial_logreg_error(Xs_te, Ys_te, model1_w)
-        err2 = multinomial_logreg_error(Xs_te, Ys_te, model2_w)
-        err3 = multinomial_logreg_error(Xs_te, Ys_te, model3_w)
-        print("\terror1: ", err1, ".\n\terror2 ", err2, "\n\terror3 ", err3)
+        # err2 = multinomial_logreg_error(Xs_te, Ys_te, model2_w)
+        # print("\terror1: ", err1, ".\n\terror2 ", err2)
 
         t1s.append(t1)
         t2s.append(t2)
-        t3s.append(t3)
+    t1s = numpy.asarray(t1s)
+    # t2s = numpy.asarray(t2s)
+    t1s_filepath = f'no_alloc_float32_1_core_implicit.npy'
+    # t2s_filepath = f'no_alloc_float32_1_core_explicit.npy'
+    numpy.save(t1s_filepath, t1s)
+    # numpy.save(t2s_filepath, t2s)
+    # plot_time_pt3(t1s, t2s, B_values)
 
-    plot_time_pt3(t1s, t2s, t3s, B_values)
+def plot_all():
+    B_values = [8, 16, 30, 60, 200, 600, 3000]
+    pyplot.figure(figsize=(12, 8))
+    t1s_single_core_filepath = 't1s_1_core.npy'
+    t2s_single_core_filepath = 't2s_1_core.npy'
+    t1s_multi_core_filepath = 't1s_4_core.npy'
+    t2s_multi_core_filepath = 't2s_4_core.npy'
+    ts_manual_threading_filepath = 'manual_threaded.npy'
+    plot6_data_filepath = 'no_alloc_float32_1_core_no_implicit.npy'
+    plot7_data_filepath = 'no_alloc_float32_1_core_explicit.npy'
+    plot8_data_filepath = 'no_alloc_float32_1_core_implicit.npy'
+    t1s_single_core = numpy.load(t1s_single_core_filepath)
+    t2s_single_core = numpy.load(t2s_single_core_filepath)
+    t1s_multi_core = numpy.load(t1s_multi_core_filepath)
+    t2s_multi_core = numpy.load(t2s_multi_core_filepath)
+    ts_manual_threading = numpy.load(ts_manual_threading_filepath)
+    p6 = numpy.load(plot6_data_filepath)
+    p7 = numpy.load(plot7_data_filepath)
+    p8 = numpy.load(plot8_data_filepath)
+    t1_plot, = pyplot.plot(np.linspace(np.min(B_values), np.max(B_values), len(B_values)), t1s_single_core)
+    t1_plot.set_label("w/ alloc w/ 1 core w/o manual threading")
+    t2_plot, = pyplot.plot(np.linspace(np.min(B_values), np.max(B_values), len(B_values)), t2s_single_core)
+    t2_plot.set_label("no alloc w/ 1 core w/o manual threading")
+    t3_plot, = pyplot.plot(np.linspace(np.min(B_values), np.max(B_values), len(B_values)), t1s_multi_core)
+    t3_plot.set_label("w/ alloc w/ 4 cores w/o manual threading")
+    t4_plot, = pyplot.plot(np.linspace(np.min(B_values), np.max(B_values), len(B_values)), t2s_multi_core)
+    t4_plot.set_label("no alloc w/ 4 cores w/o manual threading")
+    t5_plot, = pyplot.plot(np.linspace(np.min(B_values), np.max(B_values), len(B_values)), ts_manual_threading)
+    t5_plot.set_label('no alloc w/ 1 core w/ manual threading')
+    t6_plot, = pyplot.plot(np.linspace(np.min(B_values), np.max(B_values), len(B_values)), p6)
+    t6_plot.set_label('no alloc w/ 1 core w/o manual threading w/ 32-bit float')
+    t7_plot, = pyplot.plot(np.linspace(np.min(B_values), np.max(B_values), len(B_values)), p7)
+    t7_plot.set_label('no alloc w/ 1 core w/ manual threading w/ 32-bit float')
+    t8_plot, = pyplot.plot(np.linspace(np.min(B_values), np.max(B_values), len(B_values)), p8)
+    t8_plot.set_label('no alloc w/ 4 core w/o manual threading w/ 32-bit float')
+    pyplot.title("Time comparisons of preallocation and threading")
+    pyplot.xticks(np.linspace(np.min(B_values), np.max(B_values), len(B_values)), B_values)
+    pyplot.xlabel("B_values")
+    pyplot.ylabel("Time used (s)")
+    pyplot.legend()
+    pyplot.savefig("Impact of Preallocation and Threading and Precision")
+    pyplot.plot()
+    # pyplot.show()
+    # pyplot.clf()
+
 
 if __name__ == "__main__":
     (Xs_tr, Ys_tr, Xs_te, Ys_te) = load_MNIST_dataset()
-    part12(Xs_tr, Ys_tr, Xs_te, Ys_te)
+    # part12(Xs_tr, Ys_tr, Xs_te, Ys_te)
     # part3(Xs_tr, Ys_tr, Xs_te, Ys_te)
     # part4(Xs_tr, Ys_tr, Xs_te, Ys_te)
+    plot_all()
 
     # TODO add code to produce figures
